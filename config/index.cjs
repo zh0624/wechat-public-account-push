1414fc14c5814ebd9d33e435238a7b1ce0616ec1
zh0624 <60169173+zh0624@users.noreply.github.com>
2022-11-14 20:21:48 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -61,11 +61,40 @@ const USER_CONFIG = {
   USERS: [
     {
       // 想要发送的人的名字
+      name: '阿四',
+      // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
+      id: 'o40lz6nlpw1SgGSWmw8X2rdkiy2Y',
+      // 使用微信测试号：你想对他发送的模板消息的模板ID
+      useTemplateId: 'sZ3Icbx9Q95aFLhmWKBqFixOdCmL_BY5H-r6e56QoWI',
+      // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
+      horoscopeDate: '02-11',
+      horoscopeDateType: '今日',
+      festivals: [
+        // 注意：此条配置日期为阴历日期，因为`type`中 “生日” 之前有 * 符号
+        {
+          type: '*生日', name: '小猪', year: '1995', date: '02-12',
+        },
+        // 注意：此条配置日期为阳历日期，因为`type`中 “生日” 之前没有 * 符号
+        {
+          type: '生日', name: '小猪', year: '1995', date: '02-11',
+        },
+        {
+          type: '节日', name: '2周年纪念日', year: '2023', date: '09-17',
+        },
+      ],
+      // 我们在一起已经有xxxx天了的配置
+      customizedDateList: [
+        // 在一起的日子
+        { keyword: 'love_day', date: '2021-09-17' }
+      ],
+    },
+    {
+      // 想要发送的人的名字
       name: 'Zh',
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'o40lz6hM_XqunloJxyWsn_8jfOcs',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: '1ynvvYGQh1qtEjtkZE_869CgI1CF7a-wGY9wuRMTzG8',
+      useTemplateId: 'sZ3Icbx9Q95aFLhmWKBqFixOdCmL_BY5H-r6e56QoWI',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '02-11',
       horoscopeDateType: '今日',
@@ -92,7 +121,7 @@ const USER_CONFIG = {
 
 
   // 【推送完成提醒】模板id, 用来看自己有没有发送成功的那个模板
-  CALLBACK_TEMPLATE_ID: '1ynvvYGQh1qtEjtkZE_869CgI1CF7a-wGY9wuRMTzG8',
+  CALLBACK_TEMPLATE_ID: 'sZ3Icbx9Q95aFLhmWKBqFixOdCmL_BY5H-r6e56QoWI',
 
   CALLBACK_USERS: [
     {
